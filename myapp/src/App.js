13ec0be49ed2b06d2ecdf224451d8a7b72793997
Tin Buzancic <tinbuzan@gmail.com>
2020-01-14 18:17:14 -0500

    breaking down code into container components and UI components

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Ninjas from './Ninjas.js';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My first React App</h1>
-      <p>Welcome :)</p>
-      <Ninjas name="Ryu" age="25" belt="black"/>
-      <Ninjas name="Yoshi" age="35" belt="green"/>
-    </div>
-  );
+class App extends Component {
+  state = {
+    ninjas : [
+      { name: "Ryu", age: 30, belt: "black", id: 1},
+      { name: "Yoshi", age: 20, belt: "green", id: 2},
+      { name: "Crystal", age: 25, belt: "pink", id: 3}
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>My first React App</h1>
+        <p>Welcome :)</p>
+        <Ninjas ninjas={ this.state.ninjas } />
+      </div>
+    );
+  }
 }
 
 export default App;
